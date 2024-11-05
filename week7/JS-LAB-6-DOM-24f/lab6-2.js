fc3bf378a4df20a3aebb3e9741cbaa8ac9551886
Joyce Lin <ohanalin@gmail.com>
2024-11-05 11:59:09 -0500

    update

---
+++ week7/JS-LAB-6-DOM-24f/lab6-2.js
@@ -0,0 +1,48 @@
+﻿//#### LAB 6 - DOM MANIPULATION ####
+//PART 2: MYSTERY BOX
+
+//LISTEN FOR load EVENT
+window.onload = pageReady;
+
+//'WRAPPER' FUNCTION FOR DOM LOGIC
+
+
+//GET DOM ELEMENTS WE'LL NEED
+function pageReady() {
+var box = document.getElementById ("buttonBox");
+var mysterbox = document.getElementById("mysteryBox");
+
+	
+	function showBox() {
+		var answer = window.confirm("Do you want to see something?");
+		if (answer){
+			
+			box.style.display= "block";
+			mysterbox.style.display="none";	
+		}
+	
+
+	}
+	function buttonCLick(){
+	
+		box.style.background ="orange";
+		box.style.width="600px";
+		box.innerHTML ="<h2>SURPRISE!</h2>";
+	
+
+
+	}
+	box.onclick= buttonCLick;
+	mysterbox.onmousemove = showBox;
+}
+//====CREATE THE FUNCTIONS WE'LL NEED====
+//FUNCTION TO ASK USER
+
+
+//FUNCTION TO CHANGE buttonBox
+
+
+//SETUP LISTENERS
+
+
+//END onload FUNCTION
---
+++ week8/JS-LAB-7-FormValidation24F/Lab-7-forms.js
@@ -0,0 +1,59 @@
+/* LAB 7 - SHIPPING FORM */
+//Order Shipping object (for extra EXTRA challenge, otherwise, ignore it)
+
+var shipInfo = {
+	cid: 0,
+	name: "",
+	pc: "",
+	speed: "",
+	cost: 0
+};
+
+window.onload = function() {
+    // Handle form submission
+    var formHandle = document.forms.form_ship;
+    formHandle.onsubmit = processForm;
+
+    // Main function to process the form
+    function processForm() {
+        // Form elements and feedback elements
+        var hideForm = document.getElementById("shippingForm");
+        var tksform = document.getElementById("thanks_msg");
+
+        // Input fields
+        var nameValue = formHandle.f_Name;
+        var postcodeValue = formHandle.f_pc;
+        var customerSpeed = formHandle.f_speed;
+
+        // Display elements for feedback
+        var CustomerName = document.getElementById("thanksCustomer");
+        var customerPost = document.getElementById("thanksPC");
+        var customerSpeedDisplay = document.getElementById("thanksSpeed");
+        var customerCost = document.getElementById("thanksCost");
+
+        // Validate name field
+        if (nameValue.value === "") {
+            nameValue.style.background = "red";
+            return false;
+        }
+
+        // Validate postcode field
+        if (postcodeValue.value === "") {
+            postcodeValue.style.background = "red";
+            return false;
+        }
+
+        // Display values in the "Thank you" message
+        CustomerName.innerHTML = nameValue.value;
+        customerPost.innerHTML = postcodeValue.value;
+        customerSpeedDisplay.innerHTML = customerSpeed.options[customerSpeed.selectedIndex].text;
+        customerCost.innerHTML = customerSpeed.value;
+
+        // Show thank you message and hide form
+        hideForm.style.display = "none";
+        tksform.style.display = "block";
+
+        return false;
+    }
+}
+	
