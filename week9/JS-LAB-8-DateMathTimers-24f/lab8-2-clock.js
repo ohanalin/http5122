280b66d556b4725f19146189af70bca8776139dc
Joyce Lin <ohanalin@gmail.com>
2024-11-12 00:46:55 -0500

    update

--- week9/JS-LAB-8-DateMathTimers-24f/lab8-2-clock.js
+++ week9/JS-LAB-8-DateMathTimers-24f/lab8-2-clock.js
@@ -5,25 +5,54 @@
 window.onload = goNow;
 
 //create page load function
-function goNow(){
+function goNow() {
 	//create variables for required HTML elements
-	var StartBtn = document.getElementById("btnStart");
-	var StopBtn = document.getElementById("btnStop");
-	var nowHours = document.getElementById("hoursOut");
-	var nowMins = document.getElementById("minsOut");
-	var nowSecs = document.getElementById("secsOut");
+
 	
 	//create time variable so all functions have access to it
-	
+	var StartBtn = document.getElementById("btnStart");
+	var StopBtn = document.getElementById("btnStop");
+	var NowHours = document.getElementById("hoursOut");
+	var NowMins = document.getElementById("minsOut");
+	var NowSecs = document.getElementById("secsOut");
+	var timeStarts;
 	
 	//CREATE FUNCTION THAT DISPLAYS THE TIME
-	
+   
+	function time (){
+		var dateVar = new Date();
+		var Hours = dateVar.getHours();
+		var Mins = dateVar.getMinutes();
+		var Secs = dateVar.getSeconds();
+		Hours.innerHTML=NowHours;
+		newTime(Mins,NowMins);
+		newTime(Secs,NowSecs);
+	}
 	
 	//CREATE FUNCTION TO START THE CLOCK.
-	
+	function clockStart(){
+		timeStarts=setInterval(time,1000);
+		time();
+   }
+
 	
 	//CREATE FUNCTION TO STOP THE CLOCK
+	function clockStop(){
+		clearInterval(timeStarts);
+		return false;
+	}
+	function newTime(times,zero){
+		if(times<10){
+			zero.innerHTML= ":0"+times;
+		}else{
+			zero.innerHTML=":"+times;
+		}
+		
+	}
 	
 	
 	// SET EVENT LISTENERS
+	StartBtn.onclick=clockStart;
+	StopBtn.onclick=clockStop
+	time();
 }
